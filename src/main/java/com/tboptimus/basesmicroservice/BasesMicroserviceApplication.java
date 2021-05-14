a64df18703b006774690a5d0b6ce382a1acbd190
Taras Bulubas <tbulubas@gmail.com>
2021-05-14 17:31:28 +0100

    initial commit

---
+++ src/main/java/com/tboptimus/basesmicroservice/BasesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tboptimus.basesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tboptimus/basesmicroservice/BasesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tboptimus.basesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
